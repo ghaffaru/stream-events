ad5a6ef6337e35a0a000bed3b1d635cd0f091546
Ghaffaru Mudashiru <mudashiruagm@gmail.com>
2023-08-28 10:52:27 +0000

    create migrations

---
+++ database/migrations/2023_08_28_104008_create_subscribers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscribers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('name');
+            $table->enum('subscription_tier', ['Tier1', 'Tier2', 'Tier3']);
+            $table->boolean('read')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscribers');
+    }
+};
